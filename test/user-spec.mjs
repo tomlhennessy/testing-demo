a27398650f8d26ac38db2036dda8d228c4be665d
Tom Hennessy <tomlhennessy@outlook.com>
2024-06-10 20:11:54 +0100

    incorporated mocha hooks to keep test setup DRY, ensuring each test runs with a fresh instance of the 'User' class

--- test/user-spec.mjs
+++ test/user-spec.mjs
@@ -2,12 +2,17 @@
 describe('User class', function() {
     let expect;
     let User;
+    let user; // Declare 'user' in outer scope
 
     before(async function() {
         // Dynamically import chai and User class
         const chai = await import('chai');
         expect = chai.expect;
         User = (await import('../class/user.mjs')).default;
+    });
+
+    beforeEach(() => {
+        user = new User("john_doe"); // Initialise 'user' before each test
     });
 
     it('should create successfully', function() {
